ce467995c7e58e0773ad518ad04b5f4e66c95b0d
Matthew Paletta <mattpaletta@gmail.com>
2020-05-26 12:22:45 -0700

    updated benchmark image to 18.04

--- bench/langs/cpp.py
+++ bench/langs/cpp.py
@@ -4,7 +4,7 @@ from typing import List, Optional
 class CPP(object):
     def get_build_image(self, tag: Optional[str] = None) -> List[str]:
         return [
-            "FROM ubuntu:18.10" if tag is None else "FROM ubuntu:18.10 AS {0}".format(tag),
+            "FROM ubuntu:18.04" if tag is None else "FROM ubuntu:18.10 AS {0}".format(tag),
             # "RUN apt-get update -y && apt-get install -y wget && " +
             # "wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|apt-key add",
             "RUN apt-get update -y && apt-get install -y clang-7 lldb-7 lld-7 clang++-7"
